978fbbddb5cb84d89692489009c79fecb42d7e0a
Xamuelmadu <coolcatinvite@gmail.com>
2026-09-11 14:56:42 +0100

    style: update eventtech background

--- src/components/layout/Background.tsx
+++ src/components/layout/Background.tsx
@@ -1,29 +1,14 @@
-export default function Background(){
+export default function Background() {
+  return (
+    <>
+      {/* Base background */}
+      <div className="fixed inset-0 -z-50 bg-[#173E76]" />
 
-return(
+      {/* Subtle upper atmospheric glow */}
+      <div className="fixed left-1/2 top-[-250px] -z-40 h-[700px] w-[700px] -translate-x-1/2 rounded-full bg-white/5 blur-[180px]" />
 
-<>
-
-<div
-
-className="fixed inset-0 -z-50 bg-[#090909]"
-
-/>
-
-<div
-
-className="fixed left-1/2 top-[-250px] -z-40 h-[700px] w-[700px] -translate-x-1/2 rounded-full bg-yellow-400/5 blur-[180px]"
-
-/>
-
-<div
-
-className="fixed bottom-[-300px] right-[-200px] -z-40 h-[500px] w-[500px] rounded-full bg-white/5 blur-[180px]"
-
-/>
-
-</>
-
-);
-
+      {/* Subtle lower atmospheric glow */}
+      <div className="fixed bottom-[-300px] right-[-200px] -z-40 h-[500px] w-[500px] rounded-full bg-white/5 blur-[180px]" />
+    </>
+  );
 }
